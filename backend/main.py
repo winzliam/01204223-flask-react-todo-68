eb0457bb32bfe59fc6a58a9221b654696c9e40a0
Nopparuj Kartudom <winzliamcharlee@gmail.com>
2026-01-23 15:16:37 +0700

    5th Edited

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,5 @@
+from sqlalchemy import Integer, String, ForeignKey                            # เพิ่ม import Foreignkey
+from sqlalchemy.orm import Mapped, mapped_column, relationship                # เพิ่ม import relatiohship
 from sqlalchemy.orm import Mapped, mapped_column
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -24,11 +26,32 @@ class TodoItem(db.Model):
     title: Mapped[str] = mapped_column(String(100))
     done: Mapped[bool] = mapped_column(default=False)
 
+    ##### เพิ่มส่วน relationship  ซึ่งตรงนี้จะไม่กระทบ schema database เลย (เพราะว่าไม่มีการ map ไปยังคอลัมน์ใดๆ)
+    comments: Mapped[list["Comment"]] = relationship(back_populates="todo")
+
     def to_dict(self):
         return {
             "id": self.id,
             "title": self.title,
-            "done": self.done
+            "done": self.done,
+            "comments": [
+                comment.to_dict() for comment in self.comments
+            ]
+        }
+
+
+
+class Comment(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    message: Mapped[str] = mapped_column(String(250))
+    todo_id: Mapped[int] = mapped_column(ForeignKey('todo_item.id'))
+
+    todo: Mapped["TodoItem"] = relationship(back_populates="comments")
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "message": self.message,
+            "todo_id": self.todo_id
         }
 
 
