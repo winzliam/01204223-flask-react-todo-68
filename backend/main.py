8071effaa43097a123d4f588e38344c57c538cf7
Nopparuj Kartudom <winzliamcharlee@gmail.com>
2026-01-23 14:56:16 +0700

    4th Times Edited

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,5 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy import Integer, String
@@ -15,6 +17,7 @@ class Base(DeclarativeBase):
   pass
 
 db = SQLAlchemy(app, model_class=Base)
+migrate = Migrate(app, db)
 
 class TodoItem(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
@@ -28,8 +31,6 @@ class TodoItem(db.Model):
             "done": self.done
         }
 
-with app.app_context():
-    db.create_all()
 
 INITIAL_TODOS = [
     TodoItem(title='Learn Flask'),
@@ -38,9 +39,9 @@ INITIAL_TODOS = [
 
 with app.app_context():
     if TodoItem.query.count() == 0:
-         for item in INITIAL_TODOS:
-             db.session.add(item)
-         db.session.commit()
+        for item in INITIAL_TODOS:
+            db.session.add(item)
+        db.session.commit()
 
 
 todo_list = [
