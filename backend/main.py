abb2f61cb59d7037e97f1abe53b794f9118b4c8f
Nopparuj Kartudom <winzliamcharlee@gmail.com>
2026-01-23 15:49:20 +0700

    8th Edited

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,7 @@
-from sqlalchemy import Integer, String, ForeignKey                            # เพิ่ม import Foreignkey
-from sqlalchemy.orm import Mapped, mapped_column, relationship                # เพิ่ม import relatiohship
-from sqlalchemy.orm import Mapped, mapped_column
-from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy import Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from flask_migrate import Migrate
+from models import TodoItem, Comment, db   
 
 
 app = Flask(__name__)
@@ -15,56 +9,9 @@ CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
 
 
-class Base(DeclarativeBase):
-  pass
 
-db = SQLAlchemy(app, model_class=Base)
+db.init_app(app)
 migrate = Migrate(app, db)
-
-class TodoItem(db.Model):
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    title: Mapped[str] = mapped_column(String(100))
-    done: Mapped[bool] = mapped_column(default=False)
-
-    ##### เพิ่มส่วน relationship  ซึ่งตรงนี้จะไม่กระทบ schema database เลย (เพราะว่าไม่มีการ map ไปยังคอลัมน์ใดๆ)
-    comments: Mapped[list["Comment"]] = relationship(back_populates="todo")
-
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "title": self.title,
-            "done": self.done,
-            "comments": [
-                comment.to_dict() for comment in self.comments
-            ]
-        }
-
-
-
-class Comment(db.Model):
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    message: Mapped[str] = mapped_column(String(250))
-    todo_id: Mapped[int] = mapped_column(ForeignKey('todo_item.id'))
-
-    todo: Mapped["TodoItem"] = relationship(back_populates="comments")
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "message": self.message,
-            "todo_id": self.todo_id
-        }
-
-
-INITIAL_TODOS = [
-    TodoItem(title='Learn Flask'),
-    TodoItem(title='Build a Flask App'),
-]
-
-with app.app_context():
-    if TodoItem.query.count() == 0:
-        for item in INITIAL_TODOS:
-            db.session.add(item)
-        db.session.commit()
 
 
 todo_list = [
