ff9f3a4c4d24ac5f70c9940317212f3c59333e8d
Nopparuj Kartudom <winzliamcharlee@gmail.com>
2026-01-23 14:19:14 +0700

    2nd Time Edited

--- backend/main.py
+++ backend/main.py
@@ -31,6 +31,18 @@ class TodoItem(db.Model):
 with app.app_context():
     db.create_all()
 
+INITIAL_TODOS = [
+    TodoItem(title='Learn Flask'),
+    TodoItem(title='Build a Flask App'),
+]
+
+with app.app_context():
+    if TodoItem.query.count() == 0:
+         for item in INITIAL_TODOS:
+             db.session.add(item)
+         db.session.commit()
+
+
 todo_list = [
     { "id": 1,
       "title": 'Learn Flask',
@@ -42,33 +54,24 @@ todo_list = [
 
 @app.route('/api/todos/', methods=['GET'])
 def get_todos():
-    return jsonify(todo_list)
+    todos = TodoItem.query.all()
+    return jsonify([todo.to_dict() for todo in todos])
 
 def new_todo(data):
-    if len(todo_list) == 0:
-        id = 1
-    else:
-        id = 1 + max([todo['id'] for todo in todo_list])
-
-    if 'title' not in data:
-        return None
-    
-    return {
-        "id": id,
-        "title": data['title'],
-        "done": getattr(data, 'done', False),
-    }
+    return TodoItem(title=data['title'], 
+                    done=data.get('done', False))
 
 @app.route('/api/todos/', methods=['POST'])
 def add_todo():
     data = request.get_json()
     todo = new_todo(data)
     if todo:
-        todo_list.append(todo)
-        return jsonify(todo)
+        db.session.add(todo)                       # บรรทัดที่ปรับใหม่
+        db.session.commit()                        # บรรทัดที่ปรับใหม่ 
+        return jsonify(todo.to_dict())             # บรรทัดที่ปรับใหม่
     else:
         # return http response code 400 for bad requests
-        return (jsonify({'error': 'Invalid todo data'}), 400)  
+        return (jsonify({'error': 'Invalid todo data'}), 400)
     
 
 @app.route('/api/todos/<int:id>/toggle/', methods=['PATCH'])
