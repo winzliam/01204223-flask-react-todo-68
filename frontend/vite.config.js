2ea894112b8b438291dcb52516214cd182e245ed
Nopparuj Kartudom <winzliamcharlee@gmail.com>
2026-02-20 14:59:04 +0700

    add setupTests and update config 2nd time

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    test: {
+    globals: true,                          // ทำให้เรียกฟังก์ชันเกี่ยวกับการเทสได้โดยไม่ต้องประกาศ
+    environment: 'jsdom',                   // รันเทสแบบไม่มี browser
+    setupFiles: './src/setupTests.js',      // ระบุโค้ดสำหรับเตรียมต่าง ๆ
+  },
 })
+
