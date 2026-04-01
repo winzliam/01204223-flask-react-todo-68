e4d02ed08da827d97443109c7edb65260d959773
Nopparuj Kartudom <winzliamcharlee@gmail.com>
2026-04-01 15:06:48 +0700

    backend authen2

--- backend/main.py
+++ backend/main.py
@@ -4,11 +4,12 @@ from flask_migrate import Migrate
 from models import TodoItem, Comment, db 
 from models import User  
 import click
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
-
 
 
 db.init_app(app)
@@ -24,7 +25,12 @@ todo_list = [
       "done": False },
 ]
 
+
+app.config['JWT_SECRET_KEY'] = 'fdsjkfjioi2rjshr2345hrsh043j5oij5545'
+jwt = JWTManager(app)
+
 @app.route('/api/todos/', methods=['GET'])
+@jwt_required()
 def get_todos():
     todos = TodoItem.query.all()
     return jsonify([todo.to_dict() for todo in todos])
@@ -34,6 +40,7 @@ def new_todo(data):
                     done=data.get('done', False))
 
 @app.route('/api/todos/', methods=['POST'])
+@jwt_required()
 def add_todo():
     data = request.get_json()
     todo = new_todo(data)
@@ -47,6 +54,7 @@ def add_todo():
     
 
 @app.route('/api/todos/<int:id>/toggle/', methods=['PATCH'])
+@jwt_required()
 def toggle_todo(id):
     todo = TodoItem.query.get_or_404(id)
     todo.done = not todo.done
@@ -55,6 +63,7 @@ def toggle_todo(id):
 
 
 @app.route('/api/todos/<int:id>/', methods=['DELETE'])
+@jwt_required()
 def delete_todo(id):
     todo = TodoItem.query.get_or_404(id)
     db.session.delete(todo)
@@ -62,6 +71,7 @@ def delete_todo(id):
     return jsonify({'message': 'Todo deleted successfully'})
 
 @app.route('/api/todos/<int:todo_id>/comments/', methods=['POST'])
+@jwt_required()
 def add_comment(todo_id):
     todo_item = TodoItem.query.get_or_404(todo_id)
 
@@ -103,7 +113,9 @@ def login():
     if not user or not user.check_password(data['password']):
         return jsonify({'error': 'Invalid username or password'}), 401
 
-    return jsonify({'message': 'Login successful'})
+    // ***** สร้าง token แล้วคืนค่ากลับไป
+    access_token = create_access_token(identity=user.username)
+    return jsonify(access_token=access_token)
 
 
 
